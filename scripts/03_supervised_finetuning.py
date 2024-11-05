917b077f301e988e874707caf740e154e4006215
Gabriel Frontera <gabriel_frontera@hotmail.com>
2024-11-05 18:10:59 -0300

    some changes to finetuning

--- scripts/03_supervised_finetuning.py
+++ scripts/03_supervised_finetuning.py
@@ -16,52 +16,63 @@ def train_model(model, train_dataset, val_dataset, epochs=10):
 
     model.compile(
         optimizer=tf.keras.optimizers.Adam(1e-4),
-        loss=tf.keras.losses.BinaryFocalCrossentropy(apply_class_balancing=True, alpha=0.1, gamma=0.5),
-        metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()],
+        # loss=tf.keras.losses.BinaryFocalCrossentropy(apply_class_balancing=True, alpha=0.1, gamma=0.5),
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=False, label_smoothing=0.1),
+        metrics=[
+            tf.keras.metrics.BinaryAccuracy(),
+            tf.keras.metrics.Precision(),
+            tf.keras.metrics.Recall(),
+            tf.keras.metrics.AUC(multi_label=True, name="auc_roc"),
+        ],
     )
 
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', 
-                                                      min_delta=0, 
-                                                      patience=5, 
-                                                      verbose=1, 
-                                                      mode='auto')
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss", min_delta=0, patience=5, verbose=1, mode="auto"
+    )
 
-    checkpoint = tf.keras.callbacks.ModelCheckpoint("models/baseline_{epoch:02d}_{val_loss:.2f}.h5", 
-                                                    monitor='val_loss', 
-                                                    verbose=1, 
-                                                    save_best_only=True, 
-                                                    save_weights_only=True,
-                                                    mode='auto')
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        "models/finetuning_resnet18_mae/baseline_{epoch:02d}_{val_loss:.2f}.h5",
+        monitor="val_loss",
+        verbose=1,
+        save_best_only=True,
+        save_weights_only=True,
+        mode="auto",
+    )
 
-    reduceLRcallback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', 
-                                                            factor=0.5, 
-                                                            patience=3,
-                                                            verbose=1, 
-                                                            mode='auto')
+    reduceLRcallback = tf.keras.callbacks.ReduceLROnPlateau(
+        monitor="val_loss", factor=0.5, patience=3, verbose=1, mode="auto"
+    )
 
-    history = model.fit(train_dataset, validation_data=val_dataset, epochs=epochs, verbose=1,
-                        callbacks = [early_stopping, checkpoint, reduceLRcallback])
+    history = model.fit(
+        train_dataset,
+        validation_data=val_dataset,
+        epochs=epochs,
+        verbose=1,
+        callbacks=[early_stopping, checkpoint, reduceLRcallback],
+    )
     return model, history
 
 
 if __name__ == "__main__":
-    data_path = "ssl_images/data"   # "/mnt/f/ssl_images/data" if you're Gabriel 
+    data_path = "/mnt/f/ssl_images/data"  # "/mnt/f/ssl_images/data" if you're Gabriel
     data_dir = os.path.join(data_path, "processed", "pascal_voc")
     metadata_dir = os.path.join(data_path, "pascal_voc", "ImageSets", "Main")
-    pretrained_model = None # path to pretrained model if it's finetuning
+    pretrained_model = os.path.join(
+        "models", "checkpoints_resnet18_mae", "best_model.h5"
+    )  # path to pretrained model if it's finetuning
 
-    model = ResNet18((224, 224, 3), mode='classification')
-    load_encoder_weights(model, os.path.join("models", "checkpoints_resnet18_mae", "best_model.h5"))
+    model = ResNet18((224, 224, 3), mode="classification")
+    load_encoder_weights(model, pretrained_model)
     print(model.summary())
-
-    if pretrained_model:
-        print("Loading model weights...")
-        load_encoder_weights(model, pretrained_model)
 
     # Load data and create dataset
     print("Loading data and creating dataset...")
-    train_dataset = create_dataset(data_dir, split_list_file=os.path.join(metadata_dir, 'train.txt'), batch_size=32)
-    val_dataset = create_dataset(data_dir, split_list_file=os.path.join(metadata_dir, 'val.txt'), batch_size=32)
+    train_dataset = create_dataset(
+        data_dir, split_list_file=os.path.join(metadata_dir, "train.txt"), batch_size=32
+    )
+    val_dataset = create_dataset(
+        data_dir, split_list_file=os.path.join(metadata_dir, "val.txt"), batch_size=32
+    )
 
     # Train the model
     print("Training the model...")
@@ -69,7 +80,7 @@ if __name__ == "__main__":
 
     # Save the model
     os.makedirs("models", exist_ok=True)
-    save_path = os.path.join("models", "baseline_resnet18.h5")
+    save_path = os.path.join("models", "finetuning_resnet18_mae.h5")
     fine_tuned_model.save_weights(save_path)
     print(f"Final model saved to {save_path}")
 
