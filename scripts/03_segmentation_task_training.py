90d529e3e1d91f23192ab3326c9dbb7a5db39cf7
Letícia Tateishi <leticiatateishi@gmail.com>
2024-11-20 18:57:28 -0300

    updating segmentation training script

--- scripts/03_segmentation_task_training.py
+++ scripts/03_segmentation_task_training.py
@@ -6,6 +6,9 @@ import tensorflow.keras.backend as K
 import re
 import argparse
 
+os.environ['SM_FRAMEWORK'] = 'tf.keras'
+import segmentation_models as sm
+
 # Add the project root directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.models.resnet import ResNet18, load_encoder_weights
@@ -94,6 +97,154 @@ class TrainingProgressCallback(tf.keras.callbacks.Callback):
         np.save(history_path, self.history)
 
 
+def _gather_channels(x, indexes, **kwargs):
+    """Slice tensor along channels axis by given indexes"""
+    if tf.keras.backend.image_data_format() == 'channels_last':
+        x = tf.keras.permute_dimensions(x, (3, 0, 1, 2))
+        x = tf.keras.gather(x, indexes)
+        x = tf.keras.permute_dimensions(x, (1, 2, 3, 0))
+    else:
+        x = tf.keras.permute_dimensions(x, (1, 0, 2, 3))
+        x = tf.keras.gather(x, indexes)
+        x = tf.keras.permute_dimensions(x, (1, 0, 2, 3))
+    return x
+
+def gather_channels(*xs, indexes=None, **kwargs):
+    """Slice tensors along channels axis by given indexes"""
+    if indexes is None:
+        return xs
+    elif isinstance(indexes, (int)):
+        indexes = [indexes]
+    xs = [_gather_channels(x, indexes=indexes, **kwargs) for x in xs]
+    return xs
+
+
+def round_if_needed(x, threshold, **kwargs):
+    if threshold is not None:
+        x = tf.keras.greater(x, threshold)
+        x = tf.keras.cast(x, tf.keras.floatx())
+    return x
+
+
+def average(x, per_image=False, class_weights=None, **kwargs):
+    if per_image:
+        x = tf.math.reduce_mean(x, axis=0)
+    if class_weights is not None:
+        x = x * class_weights
+    return tf.math.reduce_mean(x)
+
+
+def get_reduce_axes(per_image, **kwargs):
+    axes = [1, 2] if tf.keras.backend.image_data_format() == 'channels_last' else [2, 3]
+    if not per_image:
+        axes.insert(0, 0)
+    return axes
+
+
+def iou_score(gt, pr, class_weights=1., class_indexes=None, smooth=1e-5, per_image=False, threshold=None, **kwargs):
+
+    gt, pr = gather_channels(gt, pr, indexes=class_indexes, **kwargs)
+    #pr = round_if_needed(pr, threshold, **kwargs)
+    class_indices = tf.argmax(pr, axis=-1)
+    pr = tf.one_hot(class_indices, depth=11)
+    pr = tf.cast(pr, dtype=gt.dtype)
+
+    axes = get_reduce_axes(per_image, **kwargs)
+
+    # score calculation
+    intersection = tf.math.reduce_sum(gt * pr, axis=axes)
+    union = tf.math.reduce_sum(gt + pr, axis=axes) - intersection
+
+    score = (intersection + smooth) / (union + smooth)
+    score = average(score, per_image, class_weights, **kwargs)
+
+    return score
+
+
+class IOUScore:
+
+    def __init__(
+            self,
+            class_weights=None,
+            class_indexes=None,
+            threshold=None,
+            per_image=False,
+            smooth=1e-5,
+            name=None,
+    ):
+        name = name or 'iou_score'
+        self.class_weights = class_weights if class_weights is not None else 1
+        self.class_indexes = class_indexes
+        self.threshold = threshold
+        self.per_image = per_image
+        self.smooth = smooth
+
+    def __call__(self, gt, pr):
+        return iou_score(
+            gt,
+            pr,
+            class_weights=self.class_weights,
+            class_indexes=self.class_indexes,
+            smooth=self.smooth,
+            per_image=self.per_image,
+            threshold=self.threshold
+        )
+
+class FScore:
+
+    def __init__(
+            self,
+            beta=1,
+            class_weights=None,
+            class_indexes=None,
+            threshold=None,
+            per_image=False,
+            smooth=1e-5,
+            name=None,
+    ):
+        name = name or 'f{}-score'.format(beta)
+        self.beta = beta
+        self.class_weights = class_weights if class_weights is not None else 1
+        self.class_indexes = class_indexes
+        self.threshold = threshold
+        self.per_image = per_image
+        self.smooth = smooth
+
+    def __call__(self, gt, pr):
+        return f_score(
+            gt,
+            pr,
+            beta=self.beta,
+            class_weights=self.class_weights,
+            class_indexes=self.class_indexes,
+            smooth=self.smooth,
+            per_image=self.per_image,
+            threshold=self.threshold,
+        )
+
+def f_score(gt, pr, beta=1, class_weights=1, class_indexes=None, smooth=1e-5, per_image=False, threshold=None,
+            **kwargs):
+
+
+    gt, pr = gather_channels(gt, pr, indexes=class_indexes, **kwargs)
+    class_indices = tf.argmax(pr, axis=-1)
+    pr = tf.one_hot(class_indices, depth=11)
+    pr = tf.cast(pr, dtype=gt.dtype)
+    axes = get_reduce_axes(per_image, **kwargs)
+
+    # calculate score
+    tp = tf.math.reduce_sum(gt * pr, axis=axes)
+    fp = tf.math.reduce_sum(pr, axis=axes) - tp
+    fn = tf.math.reduce_sum(gt, axis=axes) - tp
+
+    score = ((1 + beta ** 2) * tp + smooth) \
+            / ((1 + beta ** 2) * tp + beta ** 2 * fn + fp + smooth)
+    score = average(score, per_image, class_weights, **kwargs)
+
+    return score
+
+
+
 def train_model(
     model,
     train_dataset,
@@ -131,11 +282,23 @@ def train_model(
                 print("Starting training from scratch...")
                 initial_epoch = 0
 
+
+    # Segmentation models losses can be combined together by '+' and scaled by integer or float factor
+    # set class weights for dice_loss (car: 1.; pedestrian: 2.; background: 0.5;)
+    class_weights = np.array([0.5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0.5])
+    #class_weights = np.array([1., 5., 15., 11., 10., 8., 10., 5., 6., 14., 0.5])
+    dice_loss = sm.losses.DiceLoss(class_weights=class_weights) 
+    focal_loss = sm.losses.CategoricalFocalLoss()
+    total_loss = dice_loss + (1 * focal_loss)
+
+    metrics = [IOUScore(), FScore()]
+
     # Compile the model
     model.compile(
         optimizer=tf.keras.optimizers.Adam(1e-4),
-        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
-        metrics=[iou_metric],
+        loss=total_loss,
+        #loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+        metrics=metrics,
     )
 
     # Create callbacks for training
